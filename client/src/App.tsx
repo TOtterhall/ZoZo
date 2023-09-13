939e068ca5755a01b602b32342c1083cf6a8ccae
Therese Otterhäll <tessan.otterhall@gmail.com>
2023-09-13 10:33:56 +0200

    bootstrap App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Shop/Shop";
+// import { Container } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Confirmation from "../src/components/Confirmation/Confirmation";
 import UserProvider from "../context/usercontext";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Shop from "./components/Shop/Shop";
 export default function App() {
   return (
     <UserProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Shop />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/confirmation" element={<Confirmation />} />
         </Routes>
       </BrowserRouter>
