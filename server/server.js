a419e790f59ec26b1c8ce597913cb0e20ed46fd5
Therese Otterhäll <tessan.otterhall@gmail.com>
2023-09-18 16:25:18 +0200

    server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
-require("dotenv").config();
-
 const initApp = require("./middlewares");
 const app = initApp();
 const usersRouter = require("./routes/user.route");
-
+const productRouter = require("./routes/products.route");
+const checkOutRouter = require("./routes/checkout.route");
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
 //ROUTES
+app.post("/users/register", usersRouter);
 app.post("/users/login", usersRouter);
+//ROUTES_PRODUCTS
+app.get("/products", productRouter);
+app.get("/products/:id", productRouter);
+//ROUTES_CHECKOUT
+// app.get("/checkout", checkOutRouter);
+app.post("/create-checkout-session", checkOutRouter);
 
 app.listen(3040, () => console.log("Server upNRunning"));
