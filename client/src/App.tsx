cab0cf1a764322db292bf73823a2381d587c348f
Therese Otterhäll <tessan.otterhall@gmail.com>
2023-09-18 16:13:49 +0200

    App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,20 +3,28 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Confirmation from "../src/components/Confirmation/Confirmation";
 import UserProvider from "../context/usercontext";
+import ProductProvider from "../context/productcontext";
+import CartProvider from "../context/cartcontext";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Shop from "./components/Shop/Shop";
+import Cart from "./components/Cart/Cart";
 export default function App() {
   return (
     <UserProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Shop />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/confirmation" element={<Confirmation />} />
-        </Routes>
-      </BrowserRouter>
+      <ProductProvider>
+        <CartProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Shop />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/confirmation" element={<Confirmation />} />
+            </Routes>
+          </BrowserRouter>
+        </CartProvider>
+      </ProductProvider>
     </UserProvider>
   );
 }
