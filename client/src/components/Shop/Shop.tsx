0afa6beb84d0615608f08c986f46c6f95ec94bd1
Therese Otterhäll <tessan.otterhall@gmail.com>
2023-09-20 20:33:11 +0200

    Shop

--- client/src/components/Shop/Shop.tsx
+++ client/src/components/Shop/Shop.tsx
@@ -1,13 +1,11 @@
 import Navbar from "../Navbar/Navbar";
 import Products from "../Product/Product";
-// import { Row, Col } from "react-bootstrap";
 
 export default function Shop() {
   return (
     <div>
       <Navbar />
       <div>
-        <h1>VÄLKOMMEN TILL MIN SHOP</h1>
         <div className="produktlist">
           <Products />
         </div>
