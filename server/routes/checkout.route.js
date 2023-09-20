d3cf171e0e925ff713d08da972662b90a2362cca
Therese Otterhäll <tessan.otterhall@gmail.com>
2023-09-20 11:02:31 +0200

    checkoutroute_verifypayment

--- server/routes/checkout.route.js
+++ server/routes/checkout.route.js
@@ -2,7 +2,10 @@ const express = require("express");
 
 const chekOutRouter = express.Router();
 
-const { getItems } = require("../controllers/checkout.controller.js");
+const {
+  getItems,
+  verifyPayment,
+} = require("../controllers/checkout.controller.js");
 chekOutRouter.post("/create-checkout-session", getItems);
-
+chekOutRouter.post("/verify-session", verifyPayment);
 module.exports = chekOutRouter;
