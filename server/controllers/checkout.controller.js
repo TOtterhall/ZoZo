1cced4111c18e7f58675b43aa312e42550175663
Therese Otterhäll <tessan.otterhall@gmail.com>
2023-09-22 11:23:49 +0200

    checkoutcontroller

--- server/controllers/checkout.controller.js
+++ server/controllers/checkout.controller.js
@@ -1,5 +1,6 @@
 const { initStripe } = require("../stripe");
 const stripe = initStripe();
+const fs = require("fs");
 const CLIENT_URL = "http://localhost:5173";
 async function handleCart() {
   const id = req.params.id;
@@ -14,32 +15,63 @@ async function handleCart() {
     res.status(500).json({ error: "Server error" });
   }
 }
-
 async function getItems(req, res, next) {
   const { line_items } = req.body;
-  console.log(line_items);
 
   try {
     const lineItems = line_items.map((cartItem) => ({
       price: cartItem.price_data,
       quantity: cartItem.quantity,
     }));
-    console.log(lineItems);
+
     const session = await stripe.checkout.sessions.create({
       line_items: lineItems,
       mode: "payment",
       success_url: `${CLIENT_URL}/confirmation`,
       cancel_url: `${CLIENT_URL}`,
     });
-    //lagt till sessionsID
+
     res.status(200).json({ url: session.url, sessionId: session.id });
   } catch (error) {
     console.error(error.message);
     res.status(400).json("Det gick inte att skapa betalningssessionen.");
   }
 }
+
 async function verifyPayment(req, res) {
-  console.log(req.body.sessionId);
-  res.status(200).json({ verified: true });
+  try {
+    const session = await stripe.checkout.sessions.retrieve(req.body.sessionId);
+    if (session.payment_status !== "paid") {
+      return res.status(400).json({ verified: false });
+    }
+
+    const lineItems = await stripe.checkout.sessions.listLineItems(
+      req.body.sessionId
+    );
+    const order = {
+      created: session.created,
+      id: session.created,
+      customer: session.customer_details.name,
+      product: lineItems.data.map((cartItem) => {
+        return {
+          product: cartItem.description,
+          quantity: cartItem.quantity,
+          // price: cartItem.price.unit_amont / 100,
+        };
+      }),
+    };
+
+    const orderData = fs.readFileSync("db/orders.json", "utf-8");
+    const orders = JSON.parse(orderData);
+
+    orders.push(order);
+
+    fs.writeFileSync("db/orders.json", JSON.stringify(orders));
+
+    res.status(200).json({ verified: true });
+  } catch (error) {
+    console.error(error.message);
+  }
 }
+
 module.exports = { getItems, handleCart, verifyPayment };
